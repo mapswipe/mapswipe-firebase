ad623c90864862e260a59e3c3aeb2a85096f30cd
tnagorra <weathermist@gmail.com>
2025-06-23 20:07:49 +0545

    feat(typesync): add project specifics for compare, completeness, validate and validate image

--- functions/generated/pyfirebase/pyfirebase_mapswipe/models.py
+++ functions/generated/pyfirebase/pyfirebase_mapswipe/models.py
@@ -48,6 +48,18 @@ class TypesyncModel(pydantic.BaseModel):
 
 # Model Definitions
 
+class FbBaseObjCustomSubOption(TypesyncModel):
+    """Represents a custom sub option"""
+    value: int
+    description: str
+
+    class Config:
+        use_enum_values = True
+        extra = 'forbid'
+
+    def __setattr__(self, name: str, value: typing.Any) -> None:
+        super().__setattr__(name, value)
+
 class FbEnumProjectStatus(enum.Enum):
     """Represents a project status"""
     ACTIVE = "active"
@@ -77,6 +89,11 @@ class FbEnumRasterTileServerName(enum.Enum):
 class FbEnumUserGroupMembershipAction(enum.Enum):
     JOIN = "join"
     LEAVE = "leave"
+
+class FbEnumValidateInputType(enum.Enum):
+    AOI_FILE = "aoi_file"
+    LINK = "link"
+    TMID = "TMId"
 
 class FbMappingGroup(TypesyncModel):
     """Represents a group in a mapswipe project"""
@@ -151,6 +168,24 @@ class FbMappingTask(TypesyncModel):
             raise ValueError("'urlB' field cannot be set to None")
         super().__setattr__(name, value)
 
+class FbObjCustomOption(TypesyncModel):
+    """Represents a custom option"""
+    value: int
+    title: str
+    description: str
+    icon: str
+    iconColor: str
+    subOptions: typing.Union[TypesyncUndefined, typing.List[FbBaseObjCustomSubOption]] = UNDEFINED
+
+    class Config:
+        use_enum_values = True
+        extra = 'forbid'
+
+    def __setattr__(self, name: str, value: typing.Any) -> None:
+        if name == "subOptions" and value is None:
+            raise ValueError("'subOptions' field cannot be set to None")
+        super().__setattr__(name, value)
+
 class FbObjRasterTileServer(TypesyncModel):
     """Represents a raster tile server"""
     apiKey: typing.Union[TypesyncUndefined, str] = UNDEFINED
@@ -168,6 +203,32 @@ class FbObjRasterTileServer(TypesyncModel):
             raise ValueError("'apiKey' field cannot be set to None")
         if name == "wmtsLayerName" and value is None:
             raise ValueError("'wmtsLayerName' field cannot be set to None")
+        super().__setattr__(name, value)
+
+class FbProjectCompareCreateOnlyInput(TypesyncModel):
+    """Represents fields that are valid only while creating a project for COMPARE"""
+    zoomLevel: int
+    tileServer: FbObjRasterTileServer
+    tileServerB: FbObjRasterTileServer
+
+    class Config:
+        use_enum_values = True
+        extra = 'forbid'
+
+    def __setattr__(self, name: str, value: typing.Any) -> None:
+        super().__setattr__(name, value)
+
+class FbProjectCompletenessCreateOnlyInput(TypesyncModel):
+    """Represents fields that are valid only while creating a project for COMPLETENESS"""
+    zoomLevel: int
+    tileServer: FbObjRasterTileServer
+    tileServerB: FbObjRasterTileServer
+
+    class Config:
+        use_enum_values = True
+        extra = 'forbid'
+
+    def __setattr__(self, name: str, value: typing.Any) -> None:
         super().__setattr__(name, value)
 
 class FbProjectCreateOnlyInput(TypesyncModel):
@@ -193,7 +254,7 @@ class FbProjectCreateOnlyInput(TypesyncModel):
 
 class FbProjectFindCreateOnlyInput(TypesyncModel):
     """Represents fields that are valid only while creating a project for FIND"""
-    zoomLevel: typing.Union[TypesyncUndefined, int] = UNDEFINED
+    zoomLevel: int
     tileServer: FbObjRasterTileServer
 
     class Config:
@@ -201,8 +262,6 @@ class FbProjectFindCreateOnlyInput(TypesyncModel):
         extra = 'forbid'
 
     def __setattr__(self, name: str, value: typing.Any) -> None:
-        if name == "zoomLevel" and value is None:
-            raise ValueError("'zoomLevel' field cannot be set to None")
         super().__setattr__(name, value)
 
 class FbProjectReadonlyType(TypesyncModel):
@@ -247,6 +306,35 @@ class FbProjectUpdateInput(TypesyncModel):
             raise ValueError("'manualUrl' field cannot be set to None")
         if name == "teamId" and value is None:
             raise ValueError("'teamId' field cannot be set to None")
+        super().__setattr__(name, value)
+
+class FbProjectValidateCreateOnlyInput(TypesyncModel):
+    """Represents fields that are valid only while creating a project for VALIDATE"""
+    customOptions: typing.Union[TypesyncUndefined, typing.List[FbObjCustomOption]] = UNDEFINED
+    filter: str
+    inputType: FbEnumValidateInputType
+    tileServer: FbObjRasterTileServer
+
+    class Config:
+        use_enum_values = True
+        extra = 'forbid'
+
+    def __setattr__(self, name: str, value: typing.Any) -> None:
+        if name == "customOptions" and value is None:
+            raise ValueError("'customOptions' field cannot be set to None")
+        super().__setattr__(name, value)
+
+class FbProjectValidateImageCreateOnlyInput(TypesyncModel):
+    """Represents fields that are valid only while creating a project for VALIDATE"""
+    customOptions: typing.Union[TypesyncUndefined, typing.List[FbObjCustomOption]] = UNDEFINED
+
+    class Config:
+        use_enum_values = True
+        extra = 'forbid'
+
+    def __setattr__(self, name: str, value: typing.Any) -> None:
+        if name == "customOptions" and value is None:
+            raise ValueError("'customOptions' field cannot be set to None")
         super().__setattr__(name, value)
 
 class FbUserContribution(TypesyncModel):
