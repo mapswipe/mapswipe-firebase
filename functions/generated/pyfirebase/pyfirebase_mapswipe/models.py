c3053f58913dab6fe60f1136bc629949fd92a318
Sushil Tiwari <susiltiwari750@gmail.com>
2025-08-19 11:34:33 +0545

    feat(street-project): Add numberOfGroups schema on project street

--- functions/generated/pyfirebase/pyfirebase_mapswipe/models.py
+++ functions/generated/pyfirebase/pyfirebase_mapswipe/models.py
@@ -16,7 +16,7 @@ class TypesyncUndefined:
     def __init__(self):
         if TypesyncUndefined._instance is not None:
             raise RuntimeError(
-                "TypesyncUndefined instances cannot be created directly. Import and use the UNDEFINED sentinel instead."
+                "TypesyncUndefined instances cannot be created directly. Import and use the UNDEFINED sentinel instead.",
             )
         TypesyncUndefined._instance = self
 
@@ -522,7 +522,8 @@ class FbObjRasterTileServer(TypesyncModel):
 
     apiKey: str | TypesyncUndefined | None = UNDEFINED
     wmtsLayerName: typing.Annotated[
-        str | TypesyncUndefined | None, pydantic.Field(deprecated=True)
+        str | TypesyncUndefined | None,
+        pydantic.Field(deprecated=True),
     ] = UNDEFINED
     credits: str
     name: FbEnumRasterTileServerName
@@ -785,10 +786,12 @@ class FbScreen(TypesyncModel):
 
 class FbBaseTutorial(TypesyncModel):
     exampleImage1: typing.Annotated[
-        str | TypesyncUndefined | None, pydantic.Field(deprecated=True)
+        str | TypesyncUndefined | None,
+        pydantic.Field(deprecated=True),
     ] = UNDEFINED
     exampleImage2: typing.Annotated[
-        str | TypesyncUndefined | None, pydantic.Field(deprecated=True)
+        str | TypesyncUndefined | None,
+        pydantic.Field(deprecated=True),
     ] = UNDEFINED
     contributorCount: int
     informationPages: list[FbInformationPage] | TypesyncUndefined | None = UNDEFINED
