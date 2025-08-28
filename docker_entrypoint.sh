7b984777d06392ea8c60bcb169f01b07bb03fb1e
tnagorra <weathermist@gmail.com>
2025-08-28 22:11:41 +0545

    feat(docker): add env variable to disable firebase functions

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,23 +2,28 @@
 
 set -e
 
+FIREBASE_EMULATOR_FUNCTIONS_DISABLED=${FIREBASE_EMULATOR_FUNCTIONS_DISABLED:-false}
+
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FIREBASE_FUNCTIONS_DIR="$BASE_DIR/functions/"
 
+
 cd "$BASE_DIR"
 
-# NOTE: when we are using mapswipe-firebase as a submodule inside docker,
-# the .git directory is not accessible. The parent .git directory is in host
-# system not accesssible by the container
-# Inside the container, pnpm install fails when we are installing packages from git repository.
-# So, we need to switch to /tmp directory as a workaround
-echo "[INFO] Installing dependencies for transpiling functions for Firebase..."
-cd /tmp/
-pnpm --dir "$FIREBASE_FUNCTIONS_DIR" install --frozen-lockfile
+if [ $FIREBASE_EMULATOR_FUNCTIONS_DISABLED != "true" ]; then
+    # NOTE: when we are using mapswipe-firebase as a submodule inside docker,
+    # the .git directory is not accessible. The parent .git directory is in host
+    # system not accesssible by the container
+    # Inside the container, pnpm install fails when we are installing packages from git repository.
+    # So, we need to switch to /tmp directory as a workaround
+    echo "[INFO] Installing dependencies for transpiling functions for Firebase..."
+    cd /tmp/
+    pnpm --dir "$FIREBASE_FUNCTIONS_DIR" install --frozen-lockfile
 
-echo "[INFO] Transpiling functions for Firebase..."
-cd "$FIREBASE_FUNCTIONS_DIR"
-pnpm build
+    echo "[INFO] Transpiling functions for Firebase..."
+    cd "$FIREBASE_FUNCTIONS_DIR"
+    pnpm build
+fi
 
 # PIDs
 pid=0
@@ -49,8 +54,13 @@ trap graceful_shutdown SIGINT SIGTERM
 # Start Firebase emulator in background
 echo "[INFO] Starting Firebase emulator..."
 FIREBASE_EMULATOR_DATA_DIR=${FIREBASE_EMULATOR_DATA_DIR?FIREBASE_EMULATOR_DATA_DIR is required}
-firebase emulators:start --import="$FIREBASE_EMULATOR_DATA_DIR" --export-on-exit &
-pid="$!"
+if [ $FIREBASE_EMULATOR_FUNCTIONS_DISABLED != "true" ]; then
+    firebase emulators:start --import="$FIREBASE_EMULATOR_DATA_DIR" --export-on-exit &
+    pid="$!"
+else
+    firebase emulators:start --only database,hosting,auth --import="$FIREBASE_EMULATOR_DATA_DIR" --export-on-exit &
+    pid="$!"
+fi
 
 
 # Keep container alive with dummy tail
